2bf0e765f4b7d851e83a0752724409244c9a5901
Aipers-Jaegyun <yilpe@aipers.com>
2019-06-04 16:31:11 +0900

    Initial vue-basic

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+// middleware
+Vue.use(VueRouter)
+
+const Login = { template: '<div>Login Page</div>' }
+const NotFound = { template: '<div>Page not found</div>' }
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/", component: App },
+    { path: "/login", component: Login },
+    { path: "*", component: NotFound }
+  ]
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  // # route 구현
+  // computed: {
+  //   VueComponent() {
+  //     return routes[window.location.pathname] || Notfound
+  //   }
+  // },
+  // render(h) {
+  //   return h(this.VueComponent)
+  // }
+
+  router,
+  render: h => h({ template: '<router-view></router-view>' })
 }).$mount('#app')
