cbc7203184979a7df19ad7d9a5ba5459d5074914
Aipers-Jaegyun <yilpe@aipers.com>
2019-06-05 17:55:20 +0900

    Add XMLHttpRequest

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,10 @@
     Home Page
     <div>
       Board List:
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        Api result: {{ apiRes }}
+      </div>
       <ul>
         <li>
           <router-link to="/board/1">Board 1</router-link>
@@ -17,7 +21,33 @@
 
 <script>
 export default {
-  
+  data() {
+    return {
+      loading: false,
+      apiRes: ''
+    }
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+
+      // Http call (브라우저 별로 지원하지 않을 수도 있기에 jQuery의 ajax 라이브러리를 사용)
+      const req = new XMLHttpRequest();
+      req.open('GET', 'http://localhost:3000/health');
+      req.send();
+      req.addEventListener('load', () => {
+        this.loading = false;
+        this.apiRes = {
+          status: req.status,
+          statusText: req.statusText,
+          response: JSON.parse(req.response)
+        }
+      });
+    }
+  },
+  created() {
+    this.fetchData();
+  }
 }
 </script>
 
