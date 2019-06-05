94ef7bd7e5eceb5b86d100b5c8820ef5921d3528
Aipers-Jaegyun <yilpe@aipers.com>
2019-06-05 18:13:12 +0900

    Add api/board

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,8 +4,11 @@
     <div>
       Board List:
       <div v-if="loading">Loading...</div>
-      <div v-else>Api result: {{ apiRes }}</div>
-      <div v-if="error">error: {{ error }}</div>
+      <div v-else>
+        <div v-for="board in boards" :key=board.id>
+          {{board}}
+        </div>
+      </div>
       <ul>
         <li>
           <router-link to="/board/1">Board 1</router-link>
@@ -25,35 +28,19 @@ export default {
   data() {
     return {
       loading: false,
-      apiRes: '',
-      error: ''
+      boards: [],
     }
   },
   methods: {
     fetchData() {
       this.loading = true;
-
-      /*
-      // Http call (브라우저 별로 지원하지 않을 수도 있기에 jQuery의 ajax 라이브러리를 사용)
-      const req = new XMLHttpRequest();
-      req.open('GET', 'http://localhost:3000/health');
-      req.send();
-      req.addEventListener('load', () => {
-        this.loading = false;
-        this.apiRes = {
-          status: req.status,
-          statusText: req.statusText,
-          response: JSON.parse(req.response)
-        }
-      });
-      */
-
-      axios.get('http://localhost:3000/health')
+      axios.get('http://localhost:3000/boards')
         .then(res => {
-          this.apiRes = res.data;
+          this.boards = res.data
         })
-        .catch(res => {
-          this.error = res.response.data;
+        .catch(() => {
+          // 회원 인증이 안될 시 login 페이지로 리다이렉트
+          this.$router.replace('/login')
         })
         .finally(() => {
           this.loading = false;
