938dfc148d6754dca723c5729a46a115faeb8411
Aipers-Jaegyun <yilpe@aipers.com>
2019-06-05 17:44:39 +0900

    Add loading

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,26 +1,40 @@
 <template>
   <div>
     Board
-    <div>id: {{ id }}</div> 
-    <router-link :to="`/board/${id}/card/1`">Card 1</router-link>
-    <router-link :to="`/board/${id}/card/2`">Card 2</router-link>
-    <router-link :to="`/board/${id}/card/3`">Card 3</router-link>
+    <div v-if="loading">loading board...</div>
+    <div v-else>
+      <div>id: {{ id }}</div> 
+      <router-link :to="`/board/${id}/card/1`">Card 1</router-link>
+      <router-link :to="`/board/${id}/card/2`">Card 2</router-link>
+      <router-link :to="`/board/${id}/card/3`">Card 3</router-link>
+    </div>
     <hr />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        id: 0
-      }
-    },
-    created() {
-      this.id = this.$route.params.id;
+import { setTimeout } from 'timers';
+export default {
+  data() {
+    return {
+      id: 0,
+      loading: false
     }
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      setTimeout(() => {
+        this.id = this.$route.params.id;
+        this.loading = false;
+      }, 500)
+    }
+  },
+  created() {
+    this.fetchData();
   }
+}
 </script>
 
 <style lang="scss" scoped>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,25 +1,41 @@
 <template>
   <div>
     Card
-    <div>cardId: {{ cardId }}</div>
+    <div v-if="loading">loading card...</div>
+    <div v-else>cardId: {{ cardId }}</div>
   </div>
 </template>
 
 <script>
+import { setTimeout } from 'timers';
 export default {
   data() {
     return {
-      cardId: 0
+      cardId: 0,
+      loading: false
     }
   },
   watch: {
     // 감시하려는 객체
-    '$route' () {
-      this.cardId = this.$route.params.id
+    // '$route' () {
+    //   this.fetchData();
+    // }
+    '$route': {
+      handler: 'fetchData',
+      immediate: true // 즉시 실행
+    }
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      setTimeout(() => {
+        this.cardId = this.$route.params.id;
+        this.loading = false
+      }, 500)
     }
   },
   created() {
-    this.cardId = this.$route.params.id;
+    this.fetchData();
   }  
 }
 </script>
