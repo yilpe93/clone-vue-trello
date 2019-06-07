1b890e638d87d6ecaabd5eff655b56836c223c14
Aipers-Jaegyun <yilpe@aipers.com>
2019-06-07 13:39:57 +0900

    Add auth/login

--- src/api/index.js
+++ src/api/index.js
@@ -3,26 +3,35 @@ import router from '../router'
 
 const DOMAIN = 'http://localhost:3000'
 const UNAUTHORIZED = 401
-const onUnauthrorized = () => {
+const onUnauthorized = () => {
   router.push('/login');
 }
 
 const request = (method, url, data) => {
   return axios({
-    method,
-    url: DOMAIN + url,
+    method, 
+    url: DOMAIN + url, 
     data
-  }).then(result => {
-    result.data
-  }).catch(result => {
-    const { status } = result.response
-    if (status === UNAUTHORIZED) return onUnauthrorized()
-    throw Error(result)
-  })
+  }).then(result => result.data)
+    .catch(result => {
+      const {status} = result.response
+      if (status === UNAUTHORIZED) onUnauthorized()
+      throw result.response
+    })
+}
+
+export const setAuthInHeader = token => {
+  axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
 }
 
 export const board = {
   fetch() {
     return request('get', '/boards');
+  }
+}
+
+export const auth = {
+  login(email, password) {
+    return request('post', '/login', {email, password}) 
   }
 }
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,63 @@
 <template>
-  <div>
-    Login Page
+  <div class="login">
+    <h2>Log in to Trello</h2>
+    <form @submit.prevent="onSubmit">
+      <div>
+        <label for="email">Email</label>
+        <input class="form-control" type="text" name="email" v-model="email" autofocus placeholder="e.g., test@test.com" />
+      </div>
+      <div>
+        <label for="password">Passwrod</label>
+        <input class="form-control" type="password" v-model="password" placeholder="123123" />
+      </div>
+      <button class="btn" :class="{'btn-success': !invalidForm}" type="submit" :disabled="invalidForm">Log In</button>
+    </form>
+    <p class="error" v-if="error">{{ error }}</p>
   </div>
 </template>
 
 <script>
-  export default {
-    
+import { auth, setAuthInHeader } from '../api'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      returnPath: '',
+      error: ''
+    }
+  },
+  computed: {
+    invalidForm() {
+      return !this.email || !this.password
+    }
+  },
+  methods: {
+    onSubmit() {
+      auth.login(this.email, this.password)
+        .then(data => {
+          localStorage.setItem('token', data.accessToken)
+          setAuthInHeader(data.accessToken)
+          this.$router.push(this.returnPath)  // redirect Path
+        })
+        .catch(err => {
+          this.error = err.data.error
+        })
+    }
+  },
+  created() {
+    this.returnPath = this.$route.query.returnPath || '/'
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.login {
+  width: 400px;
+  margin: 0 auto; 
+}
+.error {
+  color: #f00;
+}
 </style>
